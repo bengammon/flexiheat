a79a65f8222cd25262c9f0b7cc2a3cdff566516a
Ben Gammon <ben@moov2.com>
2019-02-03 18:24:48 +0000

    Updated Google fonts stylesheet to include heavier weight for bold type

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width" />
 
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,500," rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,700," rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
