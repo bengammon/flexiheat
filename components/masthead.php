a7479dfa05d88040aa561bcc518a2a7b3b1e07e7
Ben Gammon <ben@moov2.com>
2019-04-19 11:59:07 +0100

    Moved masthead, header and footer in to their own component files

---
+++ components/masthead.php
@@ -0,0 +1,7 @@
+<div class="section section--masthead">
+    <div class="section__content">
+        <p class="text--tel">02381 221 991 &#8226; 07809 299 198</p>
+        <p class="text--hours">Mon-Sat: 6am-10pm. Sun: 7am-10pm</p>
+        <p class="text--call-out">No call out fee</p>
+    </div>
+</div>
