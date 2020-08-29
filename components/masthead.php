c0157fe05b539c3690cfe031f535393afc2b9da0
Ben Gammon <ben.gammon@etchuk.com>
2020-08-29 14:35:11 +0100

    Update masthead markup

--- components/masthead.php
+++ components/masthead.php
@@ -1,7 +1,24 @@
 <div class="section section--masthead">
     <div class="section__content">
-        <p class="text--tel">02381 221 991 &#8226; 07809 299 198</p>
-        <p class="text--hours">Mon-Sat: 6am-10pm. Sun: 7am-10pm</p>
-        <p class="text--call-out">No call out fee</p>
+
+        <ul class="list list--horizontal-spaced">
+            <li><p class="text--tel">T: 02381 221 991 &#8226; 07809 299 198</p></li>
+            <li><p class="text--hours">Mon-Sat: 6am-10pm. Sun: 7am-10pm</p></li>
+            <li><p class="text--call-out">No call out fee</p></li>
+        </ul>
+
+        <ul class="list list--horizontal-spaced-small">
+            <li>
+                <a href="https://www.facebook.com/flexibleheatingservices/" target="_blank">
+                    <img class="icon icon--facebook" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-facebook.svg" />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/flexible_heating/" target="_blank">
+                    <img class="icon icon--instagram" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-instagram.svg" />
+                </a>
+            </li>
+        </ul>
+
     </div>
 </div>
