04c9e941c9ee9205142e1363a63fb0e960702d24
Ben Gammon <ben.gammon@etchuk.com>
2020-08-29 22:13:54 +0100

    Move hero section into a component to keep homepage template clean

---
+++ components/hero.php
@@ -0,0 +1,39 @@
+<div class="section section--hero">
+    <div class="section__content">
+
+        <h1>Boiler servicing only &pound;60</h1>
+        
+        <div class="site-info">
+            <div class="google-rating">
+                <ul class="list list--horizontal">
+                    <li><img class="icon icon--star" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-star.svg" /></li>
+                    <li><img class="icon icon--star" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-star.svg" /></li>
+                    <li><img class="icon icon--star" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-star.svg" /></li>
+                    <li><img class="icon icon--star" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-star.svg" /></li>
+                    <li><img class="icon icon--star" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-star.svg" /></li>
+                </ul>
+                <p><strong>Google rating: 5.0</strong></p>
+                <p><a href="">Read our Google reviews</a></p>
+            </div>
+            <div class="site-info__description">
+                <p><em>Local gas registered engineer proudly serving Southampton, Totton, Romsey, Chandlers Ford, Hythe and the New Forest.</em></p>
+            </div>
+        </div>
+
+        <ul class="list list--horizontal offers">
+            <li><p>No call out fee <strong>Free quotations</strong></p></li>
+            <li><p>Boiler &amp; heating repairs <strong>from &pound;60</strong></p></li>
+            <li><p>Conventional boilers <strong>from &pound;1,299</strong></p></li>
+            <li><p>Combi boilers <strong>from &pound;1,599</strong></p></li>
+        </ul>
+
+        <img class="icon icon--flame icon--flame-watermark" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-flame.svg" />
+        
+    </div>
+    <div class="section__media">
+        <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/hero-1440w.jpg" media="(min-width: 376px)">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-1440w.jpg" />
+        </picture>
+    </div>
+</div>
