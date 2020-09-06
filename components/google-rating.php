19ba65c19cd9baa92134a5d6b0241121d5a62d4d
Ben Gammon <ben.gammon@etchuk.com>
2020-09-06 16:13:37 +0100

    Update google rating styles and markup

--- components/google-rating.php
+++ components/google-rating.php
@@ -7,5 +7,5 @@
         <li><img class="icon icon--star" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-star.svg" /></li>
     </ul>
     <p><strong>Google rating: 5.0</strong></p>
-    <p><a href="">Read our Google reviews</a></p>
+    <p><a href="https://www.google.com/search?q=flexible+heating+services+ltd&oq=flexible+heating+se&aqs=chrome.0.69i59j69i57.3307j0j1&sourceid=chrome&ie=UTF-8#lrd=0x48738b9fd74ce93d:0x321d13cce11eba4d,1,,," target="_blank">Read our Google reviews</a></p>
 </div>
