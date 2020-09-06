a0aed97df6d828c9b19458e0e29743e31e23d9d1
Ben Gammon <ben.gammon@etchuk.com>
2020-09-06 16:14:27 +0100

    Update footer markup

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
-<?php include_once("components/footer.php") ?>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/rellax.min.js"></script>
+
+<script>
+  // Accepts any class name
+  var rellax = new Rellax('.rellax', {
+    speed: 2,
+    center: false
+  });
+</script>
 
 <?php wp_footer(); ?>
 </body>
