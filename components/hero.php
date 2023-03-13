a15604e212e0e9d0be55ed73869af34767279395
Ben Gammon <ben.gammon@etchuk.com>
2023-03-13 22:23:08 +0000

    Update hero markup and styles

--- components/hero.php
+++ components/hero.php
@@ -1,15 +1,22 @@
 <div class="section section--hero">
     <div class="section__content">
 
-        <h1>Boiler servicing only &pound;65</h1>
+        <h1>Get a boiler installation quote</h1>
+
+        <button class="btn" id="#truequotebutton">
+            Get your quote now &raquo;
+            <small>It takes less than 30 seconds</small>
+        </button>
         
         <?php include("site-info.php") ?>
 
         <ul class="list offers">
             <li><span>Vaillant Advance</span><strong>Pro Installer</strong></li>
-            <li><span>Boiler &amp; heating repairs</span><strong>from &pound;60</strong></li>
-            <li><span>Conventional boilers</span><strong>from &pound;1,299</strong></li>
-            <li><span>Combi boilers</span><strong>from &pound;1,599</strong></li>
+            <li><span>Boiler &amp; heating repairs</span><strong>from &pound;60 <small>+VAT</small></strong></li>
+            <li><span>Conventional boilers</span><strong>from &pound;1,499 <small>+VAT</small></strong></li>
+            <li><span>Combi boilers</span><strong>from &pound;1,599 <small>+VAT</small></strong></li>
+            <li><span>Glow worm</span><strong>specialist</strong></li>
+            <li><span>5, 10 &amp; 15 year</span><strong>warranties available</strong></li>
         </ul>
 
         <img class="icon icon--flame icon--flame-watermark rellax" data-rellax-xs-speed="-4" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-flame.svg" />
